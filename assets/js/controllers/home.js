737adcaf4066ade492649405f2607ca3b44d0919
vineethyeevani <vyeevani@berkeley.edu>
2018-10-14 19:44:52 -0700

    Updated the labelling

--- assets/js/controllers/home.js
+++ assets/js/controllers/home.js
@@ -30,17 +30,19 @@ angular.module('app.controllers.home', []).controller('homePageController', func
     };
 
     $scope.add_order_test = function() {
-        add_order({table_id:"Table: 13", order_list:["dosa q: 10", "vada q: 14"], special_requests:"No onions", active_order:true});
+        var timestamp = (new Date()).getTime();
+        add_order({table_id:"Table: 13", order_list:["dosa q: 10", "vada q: 14"], special_requests:"No onions", active_order:true, timestamp: timestamp});
     };
 
     $scope.add_order = function() {
         var order_list = [];
         // Add all orders to order list
+        var timestamp = (new Date()).getTime();
         order_list = add_order_list($scope.plain_dosa, order_list, "plain dosa");
         order_list = add_order_list($scope.onion_dosa, order_list, "onion_dosa");
         order_list = add_order_list($scope.vada, order_list, "vada");
         add_order({table_id: "Table: " + $scope.table_id, order_list: order_list,
-                special_requests: $scope.special_requests, active_order:true});
+                special_requests: $scope.special_requests, active_order:true, timestamp: timestamp});
     };
 
 });
@@ -55,11 +57,15 @@ var add_order_list = function(variable, arr, type) {
 
 var sort = function(orders, status) {
     var return_dict = {};
+    var priority = 1;
     for (var key in orders) {
         if (orders[key].active_order == status) {
             return_dict[key] = orders[key];
+            return_dict[key]["priority"] = priority;
+            priority = priority + 1;
         }
     }
+
 
     return return_dict;
 }
